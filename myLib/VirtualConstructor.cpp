1597e89a6962f3e18494bd72a92a2fa860553159
royzxq <xinquanzhou@xinquans-mbp.gateway.pace.com>
2015-03-24 22:30:53 -0400

    add virtual like copy constructor

---
+++ myLib/VirtualConstructor.cpp
@@ -0,0 +1,9 @@
+//
+//  VirtualConstructor.cpp
+//  myLib
+//
+//  Created by XinquanZhou on 3/24/15.
+//  Copyright (c) 2015 Xinquan Zhou. All rights reserved.
+//
+
+#include "VirtualConstructor.h"
